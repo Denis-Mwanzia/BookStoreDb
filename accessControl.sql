226de95446ade5f881067b156ef52986e05070c4
Denis Mwanzia <denismwanzia56@gmail.com>
2025-04-13 19:16:23 +0300

    Added a grant privileges script

--- accessControl.sql
+++ accessControl.sql
@@ -3,4 +3,16 @@ CREATE ROLE 'admin';
 CREATE ROLE 'sales';
 CREATE ROLE 'customer';
 
+-- Grant Privileges to Roles
+
+-- Admin gets full access
+GRANT ALL PRIVILEGES ON BookStoreDB.* TO 'admin';
+
+-- Sales can read, insert, and update
+GRANT SELECT, INSERT, UPDATE ON BookStoreDB.* TO 'sales';
+
+-- Customer can only read
+GRANT SELECT ON BookStoreDB.* TO 'customer';
+
+
 
